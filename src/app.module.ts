1d606ac320e5d2274ce80a4171586d6dab1742a3
rafaelguerrae <rafaelguerrae@outlook.com>
2025-05-03 15:20:56 -0300

    feat(create-user): adding feature to add users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [ItemsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const userCreated = this.prisma.user.create({ data: createUserDto });
+    return userCreated;
   }
 
   findAll() {
